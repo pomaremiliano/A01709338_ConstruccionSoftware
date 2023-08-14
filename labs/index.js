3e8f9c22e40a742f6c3542afdf6c30a56138996e
emilianopomar03 <99359638+emilianopomar03@users.noreply.github.com>
2023-08-14 17:15:04 -0600

    Lab 6: Agregar ejemplo de la clase (posters)

--- labs/index.js
+++ labs/index.js
@@ -129,12 +129,12 @@ console.log(objeto.atributo1);
 
 
 
-function ejercicio1() {
+/*function ejercicio1() {
     const numero = parseInt(prompt("Ingresa un numero:"));
     
  
     for(let i = 1; i < numero; i++){
-        tablan += i,i**2,i**3;
+        let tablan = i;
 
     }
    tabla = console.log(tablan);
@@ -147,3 +147,72 @@ function ejercicio2(){
     const numero = prompt("");
 }
 ejercicio1();
+*/
+
+
+//cambia de color el texto en el input
+document.getElementById("pelicula").onkeyup = () => {
+    const red = Math.floor(Math.random() * 255);
+    const green = Math.floor(Math.random() * 255);
+    const blue = Math.floor(Math.random() * 255);
+    document.getElementById("pelicula").style.color = `rgb(${red}, ${green}, ${blue})`;
+
+}
+// arreglo de objetos para 
+const peliculas = [
+    {
+        nombre: "Titanic",
+        imagen: "https://i.etsystatic.com/27725708/r/il/3989d8/2912524873/il_fullxfull.2912524873_6vc1.jpg",
+        sinopsis: "Historia de amor entre Jack y Rose en el trasatlántico más grande de la época"
+    }, 
+    {
+        nombre: "Oppenheimer",
+        imagen: "https://m.media-amazon.com/images/M/MV5BMDBmYTZjNjUtN2M1MS00MTQ2LTk2ODgtNzc2M2QyZGE5NTVjXkEyXkFqcGdeQXVyNzAwMjU2MTY@._V1_.jpg",
+        sinopsis: "Historia de la bomba atómica"
+    }, 
+    {
+        nombre: "Barbie",
+        imagen: "https://www.instyle.com/thmb/vZCEkHB1nBMIes2tcKTUAMP0zf0=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/BarbiePosterEmbed-de7c886812184414977730e920d77a65.jpg",
+        sinopsis: "La muñeca más famosa de la historia"
+    }, 
+    {
+        nombre: "TMNT",
+        imagen: "https://mir-s3-cdn-cf.behance.net/project_modules/hd/5d830b134127975.61ce89a78d10c.jpg",
+        sinopsis: "Las aventuras flipantes de las tortugas verdes en Nueva York"
+    },
+];
+
+function genera_posters() {
+    let html = `<div class="columns">`;
+    for(let pelicula of peliculas) {
+        html += 
+        `<div class="column">
+        <div class="card">
+        <div class="card-image">
+            <figure class="image is-4by3">
+                <img src="${pelicula.imagen}" alt="${pelicula.nombre}">
+            </figure>
+        </div>
+        <div class="card-content">
+            <div class="media">
+                <div class="media-left">
+                <figure class="image is-48x48">
+                    <img src="${pelicula.imagen}" alt="${pelicula.nombre}">
+                </figure>
+                </div>
+                <div class="media-content">
+                <p class="title is-4">${pelicula.nombre}</p>
+                </div>
+            </div>
+        
+            <div class="content">
+                ${pelicula.sinopsis}
+            </div>
+        </div>
+    </div>
+</div>`;
+    }
+    html += `</div>`;
+    document.getElementById("posters").innerHTML = html;
+}
+genera_posters();
